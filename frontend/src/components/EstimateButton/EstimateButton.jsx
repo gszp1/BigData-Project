8b6f0221024ddc8a4dcfaf090192d97f75575436
gszp1 <grzybekmateusz@poczta.fm>
2024-12-08 00:51:21 +0100

    Adds reading context to button.

--- frontend/src/components/EstimateButton/EstimateButton.jsx
+++ frontend/src/components/EstimateButton/EstimateButton.jsx
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
 import styles from '@/components/EstimateButton/EstimateButton.module.css'
+import {useServerInfo} from '@/ServerContext.jsx';
+import axios from 'axios';
 
 const EstimateButton = ({carData}) => {
+    const {serverInfo, setServerInfo} = useServerInfo();
+
+    const sendData = async () => {
     
-    const sendData = () => {
     } 
     
     return (
         <>
             <div className={styles.button_container}>
-                <button className={styles.button}>
+                <button
+                    className={styles.button}
+                    onClick={sendData}
+                >
                     Estimate
                 </button>
             </div>
