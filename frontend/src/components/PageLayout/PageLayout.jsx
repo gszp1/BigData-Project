43e608b4e13ea6a0d7e3ffe04459686fc6222405
gszp1 <grzybekmateusz@poczta.fm>
2024-12-07 13:24:25 +0100

    Adds ParagraphTitle component.

--- frontend/src/components/PageLayout/PageLayout.jsx
+++ frontend/src/components/PageLayout/PageLayout.jsx
@@ -1,5 +1,6 @@
 import Banner from "/src/components/Banner/Banner.jsx";
-import TextField from "/src/components/TextField/TextField.jsx";
+// import TextField from "/src/components/TextField/TextField.jsx";
+import ParagraphTitle from "/src/components/ParagraphTitle/ParagraphTitle.jsx";
 import styles from "/src/components/PageLayout/PageLayout.module.css";
 
 const PageLayout = () => {
@@ -9,10 +10,10 @@ const PageLayout = () => {
             <div className={styles.layout}>
                 <Banner
                     bannerText={'CarWorthy'}
-                    subText={"Your Online Used Car Price Predictor"}
+                    subText={'Your Online Used Car Price Predictor'}
                 />
-                <TextField
-                    text={"Place holder"}
+                <ParagraphTitle
+                    title={'Welcome to CarWorthy - Your Trusted Used Car Price Predictor'}
                 />
             </div>
         </div>
---
+++ frontend/src/components/ParagraphTitle/ParagraphTitle.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import styles from '/src/components/ParagraphTitle/ParagraphTitle.module.css'
+
+const ParagraphTitle = ({title}) => {
+    return (
+        <>
+            <p className={styles.title}>
+                {title}
+            </p>
+        </>
+    )
+}
+
+export default ParagraphTitle;
+
+ParagraphTitle.propTypes = {
+    title: PropTypes.string.isRequired
+}
