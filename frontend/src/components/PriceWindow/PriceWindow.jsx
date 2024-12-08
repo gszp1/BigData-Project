18c349de855b59667d3e1fc71dedfda0345ca433
gszp1 <grzybekmateusz@poczta.fm>
2024-12-08 11:06:55 +0100

    Adds PriceWindow component.

---
+++ frontend/src/components/PriceWindow/PriceWindow.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import styles from '@/components/PriceWindow/PriceWindow.module.css'
+
+const PriceWindow = ({ price }) => {
+    return (
+    <>
+        <div className={styles.window}>
+            <p className={styles.window_text}>
+                Estimated price of your car:
+            </p>
+            <br/>
+            <p className={styles.price_field}>
+                {price}
+            </p>
+        </div>
+    </>
+    );
+};
+
+PriceWindow.propTypes = {
+    price: PropTypes.number.isRequired
+}
+
+export default PriceWindow;
