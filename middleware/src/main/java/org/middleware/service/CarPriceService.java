06980053cd5df2346df8c41d708faa15391c9ecc
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 22:33:30 +0100

    Adds handling received future completion.

--- middleware/src/main/java/org/middleware/service/CarPriceService.java
+++ middleware/src/main/java/org/middleware/service/CarPriceService.java
@@ -1,7 +1,10 @@
 package org.middleware.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.middleware.dto.KafkaInputMessage;
 import org.middleware.dto.PriceRequest;
+import org.middleware.dto.PriceResponse;
+import org.middleware.kafka.KafkaProducers;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -10,25 +13,40 @@ import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class CarPriceService {
 
     private final ConcurrentHashMap<String, CompletableFuture<BigDecimal>> carPriceHashMap;
 
+    private final KafkaProducers kafkaProducer;
+
     public CarPriceService(
-            @Qualifier("carPriceHashMap") ConcurrentHashMap<String, CompletableFuture<BigDecimal>> carPriceHashMap
+            @Qualifier("carPriceHashMap") ConcurrentHashMap<String, CompletableFuture<BigDecimal>> carPriceHashMap,
+            KafkaProducers kafkaProducer
     ) {
         this.carPriceHashMap = carPriceHashMap;
+        this.kafkaProducer = kafkaProducer;
     }
 
-    public Optional<BigDecimal> getCarPrice(PriceRequest request) {
+    public Optional<PriceResponse> getCarPrice(PriceRequest request) {
         var tag = UUID.randomUUID().toString();
         var kafkaMessage = KafkaInputMessage.fromPriceRequest(request, tag);
         CompletableFuture<BigDecimal> future = new CompletableFuture<>();
         carPriceHashMap.put(tag, future);
+        kafkaProducer.sendMessage(kafkaMessage);
+        log.info("CarPriceService: Waiting for result");
 
-
-        return Optional.of(new BigDecimal("2137"));
+        try {
+            BigDecimal price = future.get(60, TimeUnit.SECONDS);
+            return Optional.of(new PriceResponse(price));
+        } catch (Exception e) {
+            log.error("CarPriceService: Error occurred while waiting for car price response for tag {}", tag, e);
+            return Optional.empty();
+        } finally {
+            carPriceHashMap.remove(tag);
+        }
     }
 }
