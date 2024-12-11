da8e988cdd9912cafebcad796c29e0d80a334562
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 21:22:02 +0100

    Adds SharedHashMap bean (WIP).

---
+++ middleware/src/main/java/org/middleware/beans/SharedHashMap.java
@@ -0,0 +1,12 @@
+package org.middleware.beans;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Configuration
+public class SharedHashMap {
+    
+}
