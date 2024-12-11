fc040429e115cdec221015ac6b2d870364d94f73
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 19:41:51 +0100

    Adds KafkaInputMessage class.

--- middleware/src/main/java/org/middleware/dto/KafkaInputMessage.java
+++ middleware/src/main/java/org/middleware/dto/KafkaInputMessage.java
@@ -1,17 +1,41 @@
 package org.middleware.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
-public record KafkaInputMessage(
-        String brand,
-        String model,
-        Integer modelYear,
-        BigDecimal milage,
-        String fuelType,
-        String engine,
-        String transmission,
-        String extCol,
-        String intCol,
-        String accident,
-        String cleanTitle
-) { }
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KafkaInputMessage {
+
+    private String brand;
+    private String model;
+    private Integer modelYear;
+    private BigDecimal milage;
+    private String fuelType;
+    private String engine;
+    private String transmission;
+    private String extCol;
+    private String intCol;
+    private String accident;
+    private String cleanTitle;
+
+    public static KafkaInputMessage fromPriceRequest(PriceRequest priceRequest) {
+        return KafkaInputMessage.builder()
+                .brand(priceRequest.brand())
+                .model(priceRequest.model())
+                .modelYear(priceRequest.modelYear())
+                .milage(priceRequest.milage())
+                .fuelType(priceRequest.fuelType())
+                .engine(priceRequest.engine())
+                .transmission(priceRequest.transmission())
+                .extCol(priceRequest.extCol())
+                .intCol(priceRequest.intCol())
+                .accident(priceRequest.accident())
+                .cleanTitle(priceRequest.cleanTitle())
+                .build();
+    }
+}
--- middleware/src/main/java/org/middleware/dto/PriceResponseDto.java
+++
@@ -1,6 +0,0 @@
-package org.middleware.dto;
-
-import java.math.BigDecimal;
-
-public record PriceResponseDto(BigDecimal price) {
-}
