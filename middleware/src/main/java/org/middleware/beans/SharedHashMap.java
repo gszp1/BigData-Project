055baf713f84dcede4ea35b49c61c8a66b05a3d6
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 22:49:44 +0100

    Cleans up code.

--- middleware/src/main/java/org/middleware/beans/SharedHashMap.java
+++ middleware/src/main/java/org/middleware/beans/SharedHashMap.java
@@ -1,6 +1,5 @@
 package org.middleware.beans;
 
-import org.middleware.dto.PriceResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
