ee367821809b171865d64e7bc94a585e85380ced
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 22:48:57 +0100

    Removes redundant mapping function.

--- middleware/src/main/java/org/middleware/dto/PriceResponse.java
+++ middleware/src/main/java/org/middleware/dto/PriceResponse.java
@@ -4,10 +4,6 @@ import java.math.BigDecimal;
 
 public record PriceResponse(BigDecimal price, String message) {
 
-    public static PriceResponse fromKafkaOutputMessage(KafkaOutputMessage message) {
-        return new PriceResponse(message.getPrice(), "");
-    }
-
     @Override
     public String toString() {
         return "{" +
