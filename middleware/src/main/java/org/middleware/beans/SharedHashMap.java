b9db47526b8e9e66f581723d743d38254ac7ec39
gszp1 <grzybekmateusz@poczta.fm>
2024-12-11 21:53:03 +0100

    Adds bean for storing CompletableFuture tasks for car prices.

--- middleware/src/main/java/org/middleware/beans/SharedHashMap.java
+++ middleware/src/main/java/org/middleware/beans/SharedHashMap.java
@@ -1,8 +1,18 @@
 package org.middleware.beans;
 
+import org.middleware.dto.PriceResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class SharedHashMap {
 
+    @Bean
+    public ConcurrentHashMap<String, CompletableFuture<BigDecimal>> carPriceHashMap() {
+        return new ConcurrentHashMap<>();
+    }
 }
