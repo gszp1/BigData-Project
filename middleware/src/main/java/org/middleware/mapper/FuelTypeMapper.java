4252b5801d280b4b81e7e912dd220fa2058df26e
gszp1 <grzybekmateusz@poczta.fm>
2024-12-13 23:51:26 +0100

    Adds FuelTypeMapper class.

---
+++ middleware/src/main/java/org/middleware/mapper/FuelTypeMapper.java
@@ -0,0 +1,26 @@
+package org.middleware.mapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FuelTypeMapper {
+
+    private static Map<String, Integer> fuelTypes;
+
+    static {
+        fuelTypes = new HashMap<>();
+        fuelTypes.put("gasoline", 0);
+        fuelTypes.put("e85 flex fuel", 1);
+        fuelTypes.put("hybrid", 2);
+        fuelTypes.put("diesel", 3);
+        fuelTypes.put("plug-in hybrid", 4);
+        fuelTypes.put("not supported", 5);
+        fuelTypes.put("\u2013", 6);
+        fuelTypes.put("other", 7);
+    }
+
+    public static Integer getFuelTypeValue(String fuelType) {
+        var value = fuelTypes.get(fuelType.strip().toLowerCase());
+        return value == null ? fuelTypes.get("other") : value;
+    }
+}
